a8ed7b54b884b327e688848b55d2d22fdb66ab81
leandropio <leandro.coding@gmail.com>
2016-01-24 17:38:01 -0300

    Add project metadata

--- package.js
+++ package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'relinchos:parse-push-plugin',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Port for Meteor of a parse plugin to support push notifications',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/relinchos/relinchos-parse-push-plugin.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
@@ -16,9 +16,6 @@ Package.onUse(function(api) {
   api.addFiles('parse-push-plugin.js');
 });
 
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('relinchos:parse-push-plugin');
-  api.addFiles('parse-push-plugin-tests.js');
+Cordova.depends({
+    'medlei-parse-push-plugin': '0.1.3'
 });
